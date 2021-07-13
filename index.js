3c3d23c33726c4d47621ac01a756ebd1d328e40a
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2021-07-13 19:55:20 +0530

    Initial Lineups

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+const Cryptr = require("cryptr");
+
+function Randomizer(secret) {
+  const ts = new Date().toUTCString();
+  if (!secret || typeof secret !== "string") {
+    throw new Error("Cryptr: secret must be a non-0-length string");
+  }
+  const cryptr = new Cryptr(secret);
+
+  const encryptedTs = cryptr.encrypt(ts);
+  console.log(encryptedTs);
+  this.showKey = function showKey() {
+    return secret;
+  };
+  this.getTs = function getTs() {
+    return ts;
+  };
+}
+
+module.exports = Randomizer;
